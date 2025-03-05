838c1b387df19fb3014cbac87833485f9fc78d5d
improfessorjeff <165851431+improfessorjeff@users.noreply.github.com>
2025-03-05 16:43:40 -0600

    Update simple_traveling_salesman.py

--- simple_traveling_salesman.py
+++ simple_traveling_salesman.py
@@ -3,35 +3,37 @@
 
 from itertools import permutations
 
-# NOTE: it's possible to remove half of the paths since they're 
-# inverses of each other but that's not implemented yet
+# bruteForce: as a parameter, takes the filepath of a text file where line 1 is the 
+#   number of cities 'n' and lines 2-n+1 are a space-separated matrix of city-to-city travel costs
+# Returns a tuple (shortestPath, cost)
+def bruteForce(filepath: str):
+    matrix = []
+    input = open(filepath, 'r')
+    # takes the first line of the file as the number of cities
+    numCities = int(input.readline())
+    # stores the matrix in the file as a list of tuples
+    for line in input.readlines():
+        matrix.append([int(item) for item in line.split()])
 
-# a 2d tuple to store a graph, with its connections. Hard-coded for now
-matrix = ((0, 20, 40, 15, 29, 10),
-          (20, 0, 62, 43, 9, 8),
-          (40, 62, 0, 21, 5, 21),
-          (15, 43, 21, 0, 42, 17),
-          (29, 9, 5, 42, 0, 15),
-          (10, 8, 21, 17, 15, 0))
+    # find all possible path permutations
+    possiblePaths = list(permutations(range(1, numCities), r=numCities-1))
+    # stores the length of all paths for sorting later
+    pathLength = []
 
-numCities = 6
+    for i in possiblePaths:
+        currentLength = 0
+        # add the lengths of the first and last path
+        currentLength += matrix[0][i[0]] + matrix[0][i[-1]]
+        # add the lengths of all other paths
+        for j in range(numCities-2):
+            currentLength += matrix[i[j]][i[j+1]]
+        pathLength.append(currentLength)
 
-# find all possible path permutations
-possiblePaths = list(permutations(range(1, numCities), r=numCities-1))
-# stores the length of all paths for sorting later
-pathLength = []
-
-for i in possiblePaths:
-    currentLength = 0
-    # add the lengths of the first and last path
-    currentLength += matrix[0][i[0]] + matrix[0][i[-1]]
-    # add the lengths of all other paths
-    for j in range(numCities-2):
-        currentLength += matrix[i[j]][i[j+1]]
-    pathLength.append(currentLength)
-
-# find the shortest path
-shortestPathIndex = min(range(len(pathLength)), key=pathLength.__getitem__)
-# convert shortest path to string
-shortestPath = ",".join(str(x) for x in possiblePaths[shortestPathIndex])
-print(f'The shortest path is 0,{shortestPath},0 with a cost of {pathLength[shortestPathIndex]}')
+    # find the shortest path
+    shortestPathIndex = min(range(len(pathLength)), key=pathLength.__getitem__)
+    # convert shortest path to string
+    shortestPath = possiblePaths[shortestPathIndex]
+    cost = pathLength[shortestPathIndex]
+    return shortestPath, cost
+#print(f'The shortest path is 0,{shortestPath},0 with a cost of {pathLength[shortestPathIndex]}')
+print(bruteForce('test_matrix.txt'))
